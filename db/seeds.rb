817541c3713f9cb8ae4efa1227f74f88654db0e6
Maud Gilet <maud.gilet@gmail.com>
2018-11-21 12:50:53 +0100

    kind of ugly yelp created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attr = [
+  {
+    name: 'Gyoza Bar',
+    address: 'Passage des Panoramas',
+    category: 'japanese'
+  },
+  {
+    name: 'Little Italy',
+    address: 'Rue Montorgueil',
+    category: 'italian'
+  },
+  {
+    name: 'M. Zhao',
+    address: 'Rue des Jeuneurs',
+    category: 'chinese'
+  },
+  {
+    name: 'Dédé la Frite',
+    address: 'Rue Montmartre',
+    category: 'belgian'
+  },
+  {
+    name: 'Guy Savoy',
+    address: 'Monnaie de Paris',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attr)
